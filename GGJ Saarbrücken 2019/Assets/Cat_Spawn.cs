f6d5b15d7698cdc28c52d1675c95bbff536cccf0
Jenonator <account@jenonator.com>
2019-01-25 23:45:55 +0100

    First Scripts for the Cat

---
+++ GGJ Saarbrücken 2019/Assets/Cat_Spawn.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat_Spawn : MonoBehaviour
+{
+    public float FCatSpawnTimeMIN = 30;
+    public float FCatSpawnTimeMAX = 120;
+    float NextCatTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NextCatTime = GetNextCatTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NextCatTime -= Time.deltaTime;
+        if (NextCatTime <= 0)
+        {
+            SpawnCat();
+        }
+    }
+
+    //Setzt die Zeit bis die Katze das nächste man kommt
+    float GetNextCatTime()
+    {
+        Debug.Log("Nächste CatTime wird generiert.");
+
+        float FCatTimeGenerated = Random.Range(FCatSpawnTimeMIN, FCatSpawnTimeMAX);
+        Debug.Log("Nächste CatTime ist: " + FCatTimeGenerated + ".");
+
+        return FCatTimeGenerated;
+    }
+
+    void SpawnCat()
+    {
+        Debug.Log("Spawning Cat.");
+        //TODO Add Cat to Spawn
+        NextCatTime = GetNextCatTime();
+    }
+
+    public void StartNextCatTime()
+        {
+        Debug.Log("Next Cat Time wird gestratet.");
+        GetNextCatTime();
+        }
+}
---
+++ GGJ Saarbrücken 2019/Assets/Hand.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    Vector3 V3HandKeyPos;
+    bool BHandSlap = false;
+    public Sprite SSpriteKeys;
+    public Sprite SSpriteSlap;
+    public float fMausSpeed = 0.5f;
+    public float fMausSpeedKap;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        V3HandKeyPos = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MausH = fMausSpeed * Input.GetAxis("Mouse X");
+        float MausV = fMausSpeed * Input.GetAxis("Mouse Y");
+        float MausSpeed = MausH + MausV;
+
+        if (Input.GetKeyDown(KeyCode.K) || Input.GetKeyDown(KeyCode.L))
+        {
+            ResetHand();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && transform.position != V3HandKeyPos)
+        {
+            BHandSlap = true;
+            GetComponentInChildren<SpriteRenderer>().sprite = SSpriteSlap;
+        } else if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            BHandSlap = false;
+            GetComponentInChildren<SpriteRenderer>().sprite = SSpriteKeys;
+         
+        }
+
+        
+        
+        transform.Translate(MausH, MausV, 0); 
+        
+
+        
+
+    }
+
+
+
+    void ResetHand()
+    {
+        Debug.Log("Hand wird zurück gesetztz");
+        transform.position = V3HandKeyPos;
+    }
+
+
+}
