439bdbc70816765d4712568cc1a7bf63a53efde0
NutPea <p.nuss96@outlook.de>
2019-01-26 15:11:45 +0100

    added Full Meter , Points , DDR Gameplay

---
+++ GGJ Saarbrücken 2019/Assets/Scripts/Point_C.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Point_C : MonoBehaviour
+{
+    public Text foreGround, backGround;
+    public static int Points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Points = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreGround.text = "" + Points;
+        backGround.text = "" + Points;
+    }
+}
